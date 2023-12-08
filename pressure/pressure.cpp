5325763cc692cb5ce023f46cd29b24365fa7b389
adelinazdes <adelina.ch15@mail.com>
2023-12-08 22:56:06 +0300

    1

--- pressure/pressure.cpp
+++ pressure/pressure.cpp
@@ -1,49 +1,73 @@
 ﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+#include <clocale>
+
+#include <iomanip>
 #include <iostream>
-#include <fstream>
-#include <cmath>
 #include <vector>
-#include <windows.h>
+#include <fstream>
+#include <time.h>
+#include <algorithm>
+
+#include <fixed/fixed.h>
+#include <pde_solvers/pde_solvers.h>
+
 
 using namespace std;
 
+struct pipe
+{
+    double L, // протяженность участка
+        V, //скорость нефти
+        D, //диаметр внутр.
+        D_vnesh, //диаметр внешний
+        b, //толщина стенки
+        abc, //абсолютная шероховатость в м
+        z_0, z_L, //высотные отметки начала,конца 
+        ro, //плотность
+        u, // кинематическая вязкость Стоксы в м2/с
+        Q, //расход м3/ч
+        p_L,// давление в конце участка
+        lambda;//коэфф.гидравл.сопротивления
+};
+
+
+void pressure(struct pipe myPipe) {
+    double  p_0;
+ p_0 = (myPipe.p_L / (myPipe.ro * M_G) - myPipe.z_0 + myPipe.z_L + myPipe.lambda * (myPipe.L / myPipe.D) * pow(myPipe.V, 2) / (2 * M_G)) * (myPipe.ro * M_G);
+
+    cout << "Результат: p_0 = " << p_0 << " Па";
+    }
+
 int main()
 {
-    struct pipe
-    {
-        double L, dx, dt, V, D, b, abc, z_0, z_L, ro, u, Q;
-    };
-
+    double  Re;
+    
+    setlocale(LC_ALL, "Russian");
     //Данные по трубопроводу в СИ
     pipe myPipe;
-    double lyambda, e, p_0, p_L, D, Re;
-    p_L = 0.6e6;
+    myPipe.p_L = 0.6e6; 
     myPipe.L = 80e3;
-    myPipe.D = 720e-3;
+    myPipe.D_vnesh = 720e-3;
     myPipe.b = 10e-3;
     myPipe.z_0 = 50;
     myPipe.z_L = 100;
     myPipe.ro = 870;
-    myPipe.u = 15e-6;   //15cCт
-    myPipe.Q = 3500; //м3/ч
-    myPipe.abc = 15e-6;         //абсолютная шероховатость в м
-    D = myPipe.D - 2 * myPipe.b;
-    double Q = myPipe.Q / 3600;
-    myPipe.V = 4 * Q / (3.14 * pow(D, 2));
+    myPipe.u = 15e-6;   
+    myPipe.Q = 3500; 
+    myPipe.abc = 15e-6;  
 
-    Re = myPipe.V * D / myPipe.u;
 
-    e = myPipe.abc / D; //относ. шероховатость
-    if (Re < 2300)
-        lyambda = 64 / Re;
-    else if (Re > 500 / e) //Шифринсон
-        lyambda = 0.11 * pow(e, 0.25);
-    else if (Re > 2300) //Альтшуль
-        lyambda = 0.11 * pow((e + 68 / Re), 0.25);
+    myPipe.D = myPipe.D_vnesh - 2 * myPipe.b;
+    double Q = myPipe.Q / 3600; //перевод в м3/с
+    myPipe.V = 4 * Q / (3.14 * pow(myPipe.D, 2));
+   
+    Re = myPipe.V* myPipe.D/ myPipe.u;
+    double relative_roughness = myPipe.abc/myPipe.D;
+    myPipe.lambda = hydraulic_resistance_isaev(Re, relative_roughness);
+   
+    pressure(myPipe);
 
-    p_0 = (p_L / (myPipe.ro * 9.81) - myPipe.z_0 + myPipe.z_L + lyambda * (myPipe.L / D) * pow(myPipe.V, 2) / (2 * 9.81)) * (myPipe.ro * 9.81);
-    SetConsoleOutputCP(1251);
-    cout << "Результат: p_0 = " << p_0 << " Па";
 }
